5ef218194340a2efed74afd2adf7cc3e22366d1b
hieuhatinh <noinaycoanh094@gmail.com>
2024-06-24 07:12:19 +0700

    Exercise module 1 - week 3

---
+++ module1/week3/softmax.py
@@ -0,0 +1,33 @@
+import torch
+
+
+class Softmax(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        exp_x = torch.exp(x)
+        sum_exp_x = torch.sum(exp_x)
+        return exp_x / sum_exp_x
+
+
+class SoftmaxStable(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        c = torch.max(x)
+        exp_x = torch.exp(x - c)
+        sum_exp_x = torch.sum(exp_x)
+        return exp_x / sum_exp_x
+
+
+data = torch.Tensor([1, 2, 3])
+softmax = Softmax()
+output_softmax = softmax(data)
+print('softmax output: ', output_softmax)
+
+data = torch.Tensor([1, 2, 3])
+softmax_stable = SoftmaxStable()
+output_softmax_stable = softmax_stable(data)
+print('softmax stable output: ', output_softmax_stable)
